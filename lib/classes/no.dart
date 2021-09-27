54bb9dbce211030969707738fd8a660e64fadf85
Paulodev88 <paulo.ricardodev88@gmail.com>
2021-09-27 16:41:09 -0300

    Refactor No Class

--- lib/classes/no.dart
+++ lib/classes/no.dart
@@ -1,36 +1,8 @@
 class No {
   late int element;
-  No? left, right, dad;
+  No? left, right;
 
   No({
     required this.element,
   });
-
-  // int getElement() {
-  //   return element;
-  // }
-
-  // No? getRight() {
-  //   return right;
-  // }
-
-  // void setRight(No right) {
-  //   this.right = right;
-  // }
-
-  // No? getLeft() {
-  //   return left;
-  // }
-
-  // void setLeft(No left) {
-  //   this.left = left;
-  // }
-
-  // No getDad() {
-  //   return dad!;
-  // }
-
-  // void setDad(No dad) {
-  //   this.dad = dad;
-  // }
 }
